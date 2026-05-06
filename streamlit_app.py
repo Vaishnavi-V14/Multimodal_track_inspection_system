c8657603260cf041d1de5c7628e7d12da36099b6
Vaishnavi-V14 <vaishuvaishali4466@gmail.com>
2026-05-06 20:43:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,8 @@ from __future__ import annotations
 import sys
 import os
 
-sys.path.append(os.path.abspath("src"))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.join(BASE_DIR, "src"))
 
 import streamlit as st
 import cv2
